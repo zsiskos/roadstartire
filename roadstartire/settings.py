55f9f82483d7217faa24a5e3fbaee8f254b07832
Brandon Bernal <bernal.brandon@gmail.com>
2020-05-30 17:46:24 -0400

    Tell Django to use the customUser class

--- roadstartire/settings.py
+++ roadstartire/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Need this so that Django knows to use the new User class
+
+AUTH_USER_MODEL = 'users.CustomUser'
