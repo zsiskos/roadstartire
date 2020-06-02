b2287c7d899aa19afda035ed653000b9a1321792
Brandon Bernal <bernal.brandon@gmail.com>
2020-06-02 11:17:46 -0400

    Refactor so that User is referenced from the AUTH_USER_MODEL setting

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import CustomUser
+from django.conf import settings # Don't refer to the user model directly, it is recommended to refer to the AUTH_USER_MODEL setting
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # ────────────────────────────────────────────────────────────────────────────────
@@ -20,23 +20,18 @@ class Cart(models.Model):
     4. <strong>Fulfilled</strong> - Items have been delivered to client and payment has been received<br/>
     <br/>
     <strong>NOTE: </strong>A Cart's status must only ever progress forwards
-    """
+  """
+  
   discount_ratio_applied_help_text = """
     Must be a number from 0.00 to 1.00 (up to 2 decimal places).<br/>
     Takes the user's discount ratio when the order is submitted.
   """
 
-  user = models.ForeignKey(CustomUser, on_delete=models.CASCADE) # 1:M, a user can have many carts
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # 1:M, a user can have many carts
   date_ordered = models.DateTimeField(auto_now_add=True, verbose_name='Date Created (UTC)')
   status = models.IntegerField(choices=Status.choices, help_text=status_help_text)
   discount_ratio_applied = models.DecimalField(max_digits=4, decimal_places=2, default=0, validators=[MinValueValidator(0), MaxValueValidator(1),],)
 
-  def __str__(self):
-    return f'{self.user.first_name} {self.user.last_name} - Cart #{self.id} - {self.get_readable_status()}'
-    
-  def get_total(self):
-    pass
-  
   def get_readable_status(self):
     if self.status == 0:
       return 'Current'
@@ -47,6 +42,11 @@ class Cart(models.Model):
     elif self.status == 4:
       return 'Fulfilled'
 
+  def __str__(self):
+    return f'{self.user.first_name} {self.user.last_name} - Cart #{self.id} - {self.get_readable_status()}'
+    
+  def get_total(self):
+    pass
 
 # ────────────────────────────────────────────────────────────────────────────────
 
@@ -81,4 +81,4 @@ class CartDetail(models.Model):
     return self.quantity * Tire.price
   
   def __str__(self):
-    return f'Cart {self.cart.id} contains {self.quantity} \'{self.tire}\' tires'
+    return f'Cart {self.cart} contains {self.quantity} \'{self.tire}\' tires'
