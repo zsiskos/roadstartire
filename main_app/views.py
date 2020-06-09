d5da12c5d5991eac55a4c35a4a8f492f67dea241
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-06-09 12:42:00 -0400

    show orders by most recent

--- main_app/views.py
+++ main_app/views.py
@@ -30,7 +30,7 @@ def login(req):
 
 def account(req):
   user = req.user
-  carts = Cart.objects.filter(user_id=req.user.id)
+  carts = Cart.objects.filter(user_id=req.user.id).order_by('-date_ordered')
   return render(req, 'account.html', { 'user': user, 'carts': carts })
 
 def about(req):
