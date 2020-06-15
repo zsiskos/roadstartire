41d09632ba323ef61ceef615136b5ffa1efbd1d1
Brandon Bernal <bernal.brandon@gmail.com>
2020-06-15 11:04:22 -0400

    Add comments

--- users/managers.py
+++ users/managers.py
@@ -1,16 +1,20 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 
-# Custom user model manager where email is the unique identifiers for authentication instead of usernames
-class CustomUserManager(BaseUserManager):
+"""
+If your user model defines fields different from Django's default user, you must define a custom manager that extends BaseUserManager, 
+providing two additional methods: create_user and create_superuser
+"""
 
+# Custom user model manager where email is the unique identifier for authentication instead of usernames
+class CustomUserManager(BaseUserManager):
   # Create and save a User with the given email and password
   def create_user(self, email, password, **extra_fields):
     if not email:
       raise ValueError(_('The Email must be set'))
-    email = self.normalize_email(email)
+    email = self.normalize_email(email) # Normalizes email addresses by lowercasing the domain portion of the email address.
     user = self.model(email=email, **extra_fields)
-    user.set_password(password)
+    user.set_password(password) 
     user.save()
     return user
 
