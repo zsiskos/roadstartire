c21d2c88446f8ad715f1aad644ad04be105d981c
Brandon Bernal <bernal.brandon@gmail.com>
2020-06-28 08:13:37 -0400

    Refactor to reference Status  constant

--- main_app/signals.py
+++ main_app/signals.py
@@ -2,11 +2,11 @@ from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from .models import CartDetail
 
-# After a Tire is removed from a Cart, if the Cart is empty, delete it
+# After a CartDetail is deleted, if the Cart no longer has CartDetail objects associated with it (ie. the Cart is now empty), mark the Cart as 'ABANDONED'
 @receiver(post_delete, sender=CartDetail)
 def delete_empty_cart(sender, instance, *args, **kwargs):
   print('signal fired')
   cart = instance.cart
   if not cart.cartdetail_set.all().exists():
-    cart.status = -1 # 'Abandoned'
+    cart.status = cart.Status.ABANDONED
     cart.save()
