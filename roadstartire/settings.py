2cc11610aa4726350426cddf77067dec257b19c3
ArdyParty <ardalan.pk@gmail.com>
2020-08-06 16:50:53 -0400

    deploy test

--- roadstartire/settings.py
+++ roadstartire/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL  = '/static/'
 
 # Need this so that Django knows to use the new User class
 
