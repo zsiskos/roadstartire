dfc51a57bd2ec425c8e11e9e41ccc251aa830c02
ArdyParty <ardalan.pk@gmail.com>
2020-08-06 16:25:48 -0400

    test settings.py changes for deploy

--- roadstartire/settings.py
+++ roadstartire/settings.py
@@ -28,9 +28,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hc=@aet_2rzy+54#hnyoz)nco%fq@s8#^s%u67!z4pflgi=zdl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'www.roadstartirewholesale.ca',
+]
 
 
 # Application definition
