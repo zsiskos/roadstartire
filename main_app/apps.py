cd7198a1b0d65772280c476b10e20a4509a99059
Brandon Bernal <bernal.brandon@gmail.com>
2020-08-12 16:42:56 -0400

    Update footer and  Roadstar spelling

--- main_app/apps.py
+++ main_app/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class MainAppConfig(AppConfig):
     name = 'main_app'
-    verbose_name='Road Star Main App'
+    verbose_name='Roadstar Main App'
 
     def ready(self):
         import main_app.signals # NOTE: If just using the @receiver() decorator, just import the signals module
