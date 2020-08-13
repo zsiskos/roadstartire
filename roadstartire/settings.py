c49fabe7a1d44046fcf0f74d42c32201c756f45a
ArdyParty <ardalan.pk@gmail.com>
2020-08-13 12:26:52 -0400

    configure settings.py collect static path

--- roadstartire/settings.py
+++ roadstartire/settings.py
@@ -143,13 +143,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL  = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 
 # Need this so that Django knows to use the new User class
 
