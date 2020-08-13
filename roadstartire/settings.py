1928f007c1c75b3e44677ed2a0c22c55308e2728
ArdyParty <ardalan.pk@gmail.com>
2020-08-13 12:58:42 -0400

    added logging to settings.py and DEBUG_PROPAGATE_EXCEPTIONS = True

--- roadstartire/settings.py
+++ roadstartire/settings.py
@@ -10,6 +10,41 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
 import os
 import environ
 
