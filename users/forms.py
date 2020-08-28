a695d927b6743488568433bbd64d83b10f9cac64
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-08-28 17:32:11 -0400

    add classes to form inputs

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
-from django.forms import CharField
 
 
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+  def __init__(self, *args, **kwargs):
+      super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+      for field in self.fields:
+          self.fields[field].widget.attrs['class'] = 'form-control'
+
   class Meta(UserCreationForm):
     model = CustomUser
     fields = (
