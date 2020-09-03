18766a8853d96aa6a8bd1c172c984c935eebae86
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-09-03 17:22:19 -0400

    style forms

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,16 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
-
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.translation import gettext_lazy as _
 
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+  address = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Street address, P.O. box, c/o'}))
+  
+  address_2 = forms.CharField(label = _('Address Line 2 (optional)'), widget= forms.TextInput(attrs={'placeholder':'Apt, suite, unit, etc.'}), required = False)
+  
+  password2 = forms.CharField(label = _('Confirm password'), widget= forms.TextInput(attrs={'placeholder':'Same password as above', 'type': 'password'}))
+
   def __init__(self, *args, **kwargs):
       super(CustomUserCreationForm, self).__init__(*args, **kwargs)
       for field in self.fields:
@@ -30,12 +36,16 @@ class CustomUserCreationForm(UserCreationForm):
 class CustomUserChangeForm(UserChangeForm):
   password = None
 
+  address = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Street address, P.O. box, c/o'}))
+  
+  address_2 = forms.CharField(label = _('Address Line 2 (optional) '), widget= forms.TextInput(attrs={'placeholder':'Apt, suite, unit, etc.'}), required = False)
+
   def __init__(self, *args, **kwargs):
         super(CustomUserChangeForm, self).__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
             
-  class Meta:
+  class Meta(UserChangeForm):
     model = CustomUser
     fields = (
       'company_name', 
