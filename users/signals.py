2c5ee6940f3d22cd6e88b8220d11e9cda9d0994a
Brandon Bernal <bernal.brandon@gmail.com>
2020-10-14 14:40:44 -0400

    Update email templates

--- users/signals.py
+++ users/signals.py
@@ -3,14 +3,14 @@ from django.dispatch import receiver
 from .models import CustomUser
 from django.core.mail import send_mail
 
-# After a user is verified (ie. after a CustomUser's is_active field is set to True), notify the user via email
+# After a user is verified (ie. after a CustomUser.is_active is set to True), send them an email
 @receiver(pre_save, sender=CustomUser)
 def send_verification_email(sender, instance, *args, **kwargs):
   if CustomUser.objects.filter(pk=instance.pk).exists() and instance.is_active != CustomUser.objects.get(id=instance.id).is_active:
     if instance.is_active: # User was marked as active
       email = instance.email
-      subject = f"Your Road Star Wholesale account has been successfully verified"
-      message = f"Hello {instance.full_name} – We are glad to inform you that your Road Star Wholesale account (# {instance.pk}) has been successfully verified. \nAs a verified user, you now have access to our wide selection of tires on our website so feel free to start shopping!"
+      subject = f"Your Roadstar Tire Wholesale account was successfully verified"
+      message = f"Hello {instance.full_name} – We are happy to inform you that your Roadstar Tire Wholesale account was successfully verified.\nAs a verified user, you have access to our wide selection of tires on our website.\n\nLog in: {instance.email}"
       send_mail(
         subject, 
         message, 
@@ -20,8 +20,8 @@ def send_verification_email(sender, instance, *args, **kwargs):
       )
     else: # User was marked as inactive
       email = instance.email
-      subject = f"Your Road Star Wholesale account was recently disabled"
-      message = f"Hello {instance.full_name} – This is an email confirmation to let you know that your Road Star Wholesale account (# {instance.pk}) has been disabled. \nYou will be unable to log in again until and admin verifies your account again."
+      subject = f"Your Roadstar Tire Wholesale account is inactive"
+      message = f"Hello {instance.full_name} – This is an email confirmation to inform you that your Roadstar Tire Wholesale account was recently inactvated.\nYou will be unable to log in until a staff member verifies your account again."
       send_mail(
         subject, 
         message, 
